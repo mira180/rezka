8aac392852dd700001fa998870fa0d6e5203e43c
Mira <mira180@list.ru>
2021-12-14 15:20:26 +0300

    og fix

--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ import logging
 from db import Database
 from rezka import Rezka
 
-from .config import DB_URI, TIME_PATTERN, UPDATE_INTERVAL, TYPE_MOVIE
+from .config import DB_URI, TIME_PATTERN, UPDATE_INTERVAL
 
 logger = logging.getLogger(__name__)
 
@@ -85,7 +85,7 @@ def watch(id_):
         db.update('items', {'_id': id_}, new_values)
         item = db.find('items', {'_id': id_})
     og = {
-        "title": f"{item['title']} – смотреть {'фильм' if item['type'] == TYPE_MOVIE else 'сериал'}",
+        "title": f"{item['title']}",
         "description": item["about"],
         "image": item["cover"],
         "url": request.base_url
